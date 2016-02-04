07919e330d6e955f9b896ba4058359298bec8d44
Daniel J. Hofmann <daniel@trvx.org>
2016-02-04 15:17:27 +0100

    Removes mapped file abstraction

--- core.cc
+++ core.cc
@@ -4,7 +4,6 @@
 
 #include <boost/timer/timer.hpp>
 
-#include "mapped_file.h"
 #include "file_mapping.h"
 #include "mincore.h"
 #include "blob.h"
@@ -17,31 +16,25 @@ int main(int argc, char** argv) try {
 
   // dump_to_binary(argv[1]);
 
-  // This uses Boost.Iostream's mapped_file abstraction.
-  // Does not support madvise, or anything more than just a simple mmap use-case.
-  // const auto region = make_mapped_file(argv[1]);
+  // 1/ read from file into heap
+  // const auto vec = read_from_binary(argv[1]);
+  // const auto first = vec.data();
+  // const auto last = vec.data() + vec.size();
 
-  // This uses Boost.Interprocess' file_mapping abstraction.
-  // Supports setting madvise via enum and passing flags to the underlying mmap.
-  // const auto region = make_file_mapping(argv[1]);
+  // 2/ read directly through mmaped region
+  const auto region = make_file_mapping(argv[1]);
 
   // dump_mincore(region);
 
-  // 1/ read from file into heap
-  const auto vec = read_from_binary(argv[1]);
-  const auto first = vec.data();
-  const auto last = vec.data() + vec.size();
-
-  // 2/ read directly through mmaped region
-  // const auto first = static_cast<const Blob*>(region.get_address());
-  // const auto bytes = region.get_size();
-  // const auto last = first + (bytes / sizeof(Blob));
+  const auto first = static_cast<const Blob*>(region.get_address());
+  const auto bytes = region.get_size();
+  const auto last = first + (bytes / sizeof(Blob));
 
   {
     boost::timer::auto_cpu_timer _;
 
-    access_seq(first, last);
-    // access_rnd(first, last);
+    // access_seq(first, last);
+    access_rnd(first, last);
   }
 
 } catch (const std::exception& e) {
